284f198ca85f48abd3e7c38605931561123cff86
srsanjaykumar <mesrsanjay@gmail.com>
2023-12-21 12:15:00 +0530

    Fixing Chakra Menu issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Grid, GridItem, HStack, Show } from "@chakra-ui/react"
+import { Box, Flex, Grid, GridItem, HStack, Show } from "@chakra-ui/react"
 import NavBar from "./components/NavBar"
 import GameGrid from "./components/GameGrid"
 import GenreList from "./components/GenreList"
@@ -51,14 +51,16 @@ function App() {
       </Show>
 
       <GridItem area={'main'} >
-        <HStack spacing={5} padding={2} marginBottom={5}>
-          <PlatformSelector selectedPlatform={gameQuery.platform} onSelectPlatform={(platform) => setGameQuery({ ...gameQuery, platform })} />
-          <SortSelector 
-          sortOrder={gameQuery.sortOrder}  // Get Data in GameQuery 
-          onSelectSortOrder={(sortOrder) => {
-            setGameQuery({ ...gameQuery, sortOrder })  // Set Data into GameQuery 
-          }} />
-        </HStack>
+        <Flex padding={2} marginBottom={5}>
+          <Box marginRight={5}>
+            <PlatformSelector selectedPlatform={gameQuery.platform} onSelectPlatform={(platform) => setGameQuery({ ...gameQuery, platform })} />
+            <SortSelector
+              sortOrder={gameQuery.sortOrder}  // Get Data in GameQuery 
+              onSelectSortOrder={(sortOrder) => {
+                setGameQuery({ ...gameQuery, sortOrder })  // Set Data into GameQuery 
+              }} />
+          </Box>
+        </Flex>
         <GameGrid gameQuery={gameQuery} />
       </GridItem>
     </Grid>
