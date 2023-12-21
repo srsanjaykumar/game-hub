da95071bf88e7d222fcbee603ae06494334f83d2
srsanjaykumar <mesrsanjay@gmail.com>
2023-12-21 12:08:16 +0530

    Handling Games WithOut a Image

--- src/services/image-utl.ts
+++ src/services/image-utl.ts
@@ -1,11 +1,13 @@
+import noImage from '../assets/no-image-placeholder.webp'
+
 const getCroppedImageUrl = (url: string) => {
     if (!url) {
-        return ' '
+        return noImage
     }
     const target = 'media/'
     const index = url.indexOf(target) + target.length;
-   return  url.slice(0, index) +'crop/600/400/'+url.slice(index)
-    
+    return url.slice(0, index) + 'crop/600/400/' + url.slice(index)
+
 }
 
 export default getCroppedImageUrl;
